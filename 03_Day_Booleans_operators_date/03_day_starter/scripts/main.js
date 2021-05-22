f412520739716d6d71d1b622d580b33869785ba5
khaled osman <khaledosman737@gmail.com>
2021-05-22 09:47:32 +0200

    .

--- 03_Day_Booleans_operators_date/03_day_starter/scripts/main.js
+++ 03_Day_Booleans_operators_date/03_day_starter/scripts/main.js
@@ -1,1 +1,6 @@
 // this is your main.js script
+
+console.log(Math.log(Math.E))
+let my_name = "khAled hAmadhamahama".repeat(10)
+// let new_name = my_name.replaceAll(/\a+/g,'A')
+console.log(my_name.match(/ha/gi).length)
