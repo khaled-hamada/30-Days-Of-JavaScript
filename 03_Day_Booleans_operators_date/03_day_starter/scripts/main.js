92df942e280f8162956a44fab0c4a921bc64dba4
khaled osman <khaledosman737@gmail.com>
2021-05-23 14:46:27 +0200

    .

--- 03_Day_Booleans_operators_date/03_day_starter/scripts/main.js
+++ 03_Day_Booleans_operators_date/03_day_starter/scripts/main.js
@@ -1,6 +1,30 @@
 // this is your main.js script
 
-console.log(Math.log(Math.E))
-let my_name = "khAled hAmadhamahama".repeat(10)
-// let new_name = my_name.replaceAll(/\a+/g,'A')
-console.log(my_name.match(/ha/gi).length)
+// console.log(Math.log(Math.E))
+// let my_name = "khAled hAmadhamahama".repeat(10)
+// // let new_name = my_name.replaceAll(/\a+/g,'A')
+// console.log(my_name.match(/ha/gi).length)
+
+// let input_number = prompt("enter a number between 0 and 10","number goes here")
+// console.log(`user entered text ${input_number}`)
+// console.log(`user entered extracted number ${parseFloat(input_number)}`)
+
+// let your_name = prompt("enter your name")
+// let condition = confirm("do you want to print your name to console or not ?!")
+
+// condition ? console.log(`your name is ${your_name}`) : console.log("a")
+
+let now = new Date()
+console.log(now)
+console.log(` year is : ${now.getFullYear()}`)
+console.log(` month is : ${now.getMonth() + 1}`)
+console.log(` day is : ${now.getDate()}`)
+console.log(` hour is : ${now.getHours()}`)
+console.log(` minute is : ${now.getMinutes()}`)
+console.log(` seconds is : ${now.getSeconds()}`)
+console.log(` day number is : ${now.getDay() + 1   }`)
+
+
+// console.log(!(4 === '4'))
+
+// console.log( !('python'.includes('on') && 'jargon'.includes('on') )  )
--- 04_Day_Conditionals/04_day_starter/scripts/main.js
+++ 04_Day_Conditionals/04_day_starter/scripts/main.js
@@ -1,3 +1,29 @@
-// this is your main.js script
 
-alert('Open the browser console whenever you work on JavaScript')
+let month = prompt("Enter a month name").toLowerCase()
+
+
+console.log(month)
+
+
+switch(month){
+    case 'Janurary':
+        console.log(`${month} is 31 days`)
+        break 
+    case 'february':
+    case 'feb':
+    case'f':
+        let year = new Date().getFullYear()
+        if ((year % 4 == 0) || (year % 100 == 0)) {
+             console.log(`${month} is 29 days`)
+        }
+        else{
+             console.log(`${month} is 28 days`)
+        }
+        break 
+   
+    default :
+        console.log("it is not a number ")
+        break 
+
+}
+
